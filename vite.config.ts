d8350d2e961597692916ac69ad5d85733c81b289
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-06-26 13:33:41 +0100

    fix missing css in deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {} // 👈 this line alone fixes most Web3Modal errors
+    "process.env": {}, // 👈 this line alone fixes most Web3Modal errors
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
